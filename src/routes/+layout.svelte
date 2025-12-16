13d2f1467f7b246ba86a0296cf6a72e5addcaa23
Welton <welton.carvalho.dev@gmail.com>
2025-12-16 15:58:43 -0300

    feat: create header site

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,7 +6,22 @@
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
-<header>
-
+<header class="h-[100px] w-screen pl-[50px] pr-[50px] bg-white flex justify-between items-center">
+    <div class="flex gap-[5px] items-center font-logo text-logo">
+        <img src="/logo.svg" alt="User" />
+        Furniro
+    </div>
+    <nav class="w-[430px] flex justify-between items-center font-navbar text-navbar">
+        <a href="/">Home</a>
+        <a href="/shop">Shop</a>
+        <a href="/about">About</a>
+        <a href="/contact">Contact</a>
+    </nav>
+    <div class="flex gap-10">
+        <a href="/" class="size-[28px]"><img src="/logout.svg" alt="logout button"></a>
+        <a href="/" class="size-[28px]"><img src="/search.svg" alt="logout button"></a>
+        <a href="/" class="size-[28px]"><img src="/heart.svg" alt="logout button"></a>
+        <a href="/" class="size-[28px]"><img src="/cart.svg" alt="logout button"></a>
+    </div>
 </header>
 {@render children()}
